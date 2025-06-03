c750a0d70a39a83a34178d2c2aeaf63d8559e3a0
paz.l.angel <paz.l.angel@gmail.com>
2025-06-03 00:46:00 +0000

    consumAPI

---
+++ src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,22 @@
+package com.aluracursos.screenmatch;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.aluracursos.screenmatch.service.ConsumoAPI;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ScreenmatchApplication.class, args);
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    var consumoApi = new ConsumoAPI();
+    var json = consumoApi.obtenerDatos("https://www.omdbapi.com/?t=game+of+thrones&&apikey=4fc7c187");
+    System.out.println(json);
+  }
+}
